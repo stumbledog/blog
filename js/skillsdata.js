968dce0f406ba0af8bb6b60ec6feb60081825abe
stumbledog <stumbledog@gmail.com>
2015-04-25 22:22:25 -0400

    first commit

---
+++ js/skillsdata.js
@@ -0,0 +1,45 @@
+var skillsdata;
+
+skillsdata = {
+  "Skills":{
+    "Web Development":{
+      "Server Side": {
+        "Node.js":{
+          "express": [0,0,0,30,50],
+          "socket.io": [0,0,0,20,30],
+          "npm": [0,0,0,20,30],
+        },
+        "PHP":{
+          "PHP":[0,0,10,30,50],
+          "Laravel":[0,0,0,20,40],
+        },
+      },
+      "Front Side":{
+        "HTML": [10,30,50,70,80],
+        "CSS": {
+          "CSS": [0,10,50,70,80],
+          "Bootstrap": [0,0,20,50,70],
+          "Responsive": [0,0,20,50,70],
+        },
+        "JSFramework": {
+          "jQuery": [0,0,30,60,80],
+          "BackboneJs": [0,0,30,50,50],
+          "CreateJs": [0,0,0,50,80],
+        },
+        "Template": {
+          "Jade": [0,0,0,20,40],
+        }
+      }
+    },
+    "Language": {
+      "Javascript": [0,20,40,60,80],
+      "Java": [50,50,50,50,50],
+      "C/C++": [30,30,30,30,30],
+    },
+    "Database":{
+      "Informix":[0, 0, 20, 40, 60],
+      "MySQL":[0, 0, 20, 40, 60],
+      "MongoDB":[0, 0, 0, 30, 60],
+    },
+  }
+};
